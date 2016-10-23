094f2ee4c92ddf285817f5ca2b595eacd25e1ae5
mruis <caliba@gmail.com>
2016-10-23 22:37:38 +0300

    style fixes

--- client/components/SongContainer.js
+++ client/components/SongContainer.js
@@ -42,6 +42,34 @@ class SongContainer extends React.Component {
         this.handleNextClick();
     }
 
+    definePrevButton() {
+        let style = "left";
+        if (this.state.songNumber == 0) {
+            style = style + " disabled";
+        }
+        return <PrevNextButton style={style} enabled={true} onClick={this.handlePrevClick.bind(this)}/>;
+    }
+
+    defineNextButton() {
+        let style = "right";
+        if (this.state.songNumber == MAX_SONGS-1) {
+            style = style + " disabled";
+        }
+        return <PrevNextButton style={style} onClick={this.handleNextClick.bind(this)}/>;
+    }
+
+    defineSoundElement(preview_url) {
+        if (preview_url != null) {
+            return (<Sound url={preview_url}
+                           playStatus={Sound.status.PLAYING}
+                           volume={this.state.volume}
+                           onFinishedPlaying={this.handleSongFinished.bind(this)} />
+                    );
+        } else {
+            return null;
+        }
+    }
+
     render() {
         const { loading, items } = this.props.songs;
         const { album, artists, name, preview_url } = items[this.state.songNumber];
@@ -51,25 +79,20 @@ class SongContainer extends React.Component {
             return <h2>Loading...</h2>;
         }
 
-        var soundElement = null;
-        if (preview_url != null) {
-            soundElement = <Sound url={preview_url}
-                                  playStatus={Sound.status.PLAYING}
-                                  volume={this.state.volume}
-                                  onFinishedPlaying={this.handleSongFinished.bind(this)} />
-        }
+        const soundElement = this.defineSoundElement(preview_url);
+        const prevButton = this.definePrevButton();
+        const nextButton = this.defineNextButton();
 
         return (
             <div className="content">
                 <h2>Here, have some earworms.</h2>
-                <PrevNextButton style="left" onClick={this.handlePrevClick.bind(this)}/>
+                {prevButton}
                 <Song artist={artists[0].name} song={name} url={imageUrl} />
                 {soundElement}
-                <PrevNextButton style="right" onClick={this.handleNextClick.bind(this)}/>
+                {nextButton}
             </div>
         );
     }
-
 
 }
 
