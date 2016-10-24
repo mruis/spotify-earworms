0aabd3ac72e3656bd8cb7af8357793639af95c37
mruis <caliba@gmail.com>
2016-10-24 22:14:50 +0300

    oh so funny

--- client/components/Login.js
+++ client/components/Login.js
@@ -7,7 +7,7 @@ export default class Login extends React.Component {
   render() {
     return (
       <div className="login">
-        <h2 className="login-text">Log in below to see your Spotify Earworms!</h2>
+        <h2 className="login-text">You have some earworms waiting for you. Log in below to see them!</h2>
         <a className="login-button" href="/login"><img src={loginImage}/></a>
       </div>
     );
