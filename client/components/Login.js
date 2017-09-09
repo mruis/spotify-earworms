deb714d493b1b5bd955d0390c3c12b390fa322de
mruis <caliba@gmail.com>
2017-09-09 17:35:55 +0300

    don't worry, your security is all right

--- client/components/Login.js
+++ client/components/Login.js
@@ -9,6 +9,9 @@ export default class Login extends React.Component {
       <div className="login">
         <h2 className="login-text">You have some earworms waiting for you. Log in below to see them!</h2>
         <a className="login-button" href="/login"><img src={loginImage}/></a>
+		<br/><br/><br/>
+		<p>We use a secured connection and only some of your past top songs <br/>are fetched, so no worries regarding your account safety 😊</p>
+		<p>Made by <a href="https://github.com/mruis">Miika Ruissalo</a>, 2016</p>
       </div>
     );
   }
